9f7b82e1e738cd52e40ead4b56e5c59d451cbc73
aivars <aiva0071@stud.kea.dk>
2020-10-16 01:40:59 +0200

    position of the title/gsap animation on slide change

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,38 @@
 import "../scss/style.scss";
 import fullpage from "fullpage.js";
-
+import { gsap } from "gsap";
+import { Power4 } from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
+gsap.registerPlugin(TextPlugin);
 
 window.addEventListener("DOMContentLoaded", start);
 
-function start(){
+function start() {
 
     new fullpage('#fullpage', {
         scrollBar: true,
-        scrollingSpeed: 600
+        scrollingSpeed: 300,
+
+        onLeave: function () {
+
+            console.log("hey");
+            const tml = gsap.timeline();
+
+            tml.to(".section__text", 0.2, {
+                opacity: 0,
+                ease: Power4.easeOut
+            })
+
+            tml.to(".section__text", 0.2, {
+                opacity: 1,
+                ease: Power4.easeOut
+            })
+
+            gsap.set("h1", {
+                delay: 0.2,
+                text: "The Algae Dome"
+            })
+        }
     })
 
 }
